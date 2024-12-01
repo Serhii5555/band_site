11b835b0ac5d066b0b87de7f28088f9d7a482d14
BeilakhSerhii <ipz233_bsv@student.ztu.edu.ua>
2024-12-01 16:14:24 +0200

    Fixed some pages styling

--- app.py
+++ app.py
@@ -103,6 +103,10 @@ def logout():
     flash('Logged out successfully!', 'success')
     return redirect(url_for('home'))
 
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # Creates tables if they don't exist
